12b9de8ed04bb91a207f5cc8785294731c97c48a
s poorna chandra <poornanaidu1@gmail.com>
2023-12-27 12:47:16 +0530

    practice

--- module/main.tf
+++ module/main.tf
@@ -15,21 +15,21 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.instances.private_ip]
 }
 
-#resource "null_resource" "ansible" {
-#
-#  depends_on = [
-#    aws_route53_record.record
-#  ]
-#
-#  provisioner "local-exec" {
-#    command = <<EOF
-#cd /home/centos/practice/ansible-roboshop
-#git pull
-#sleep 30
-#ansible-playbook -i ${var.name}-dev.poornadevops.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
-#EOF
-#  }
-#}
+resource "null_resource" "ansible" {
+
+  depends_on = [
+    aws_route53_record.record
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/practice/ansible-roboshop
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.poornadevops.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+EOF
+  }
+}
 
 
 
