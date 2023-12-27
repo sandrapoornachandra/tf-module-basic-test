58b6bca7c6d743823378ec55987be175bf808022
s poorna chandra <poornanaidu1@gmail.com>
2023-12-27 12:26:07 +0530

    practice

--- main.tf
+++ main.tf
@@ -1,15 +1,35 @@
-module "components" {
-
-  for_each = var.components
-
-  source          = "./module"
-  zone_id         = var.zone_id
-  security_groups = var.security_groups
-  name            = each.value["name"]
-  instance_type   = each.value["instance_type"]
+resource "aws_instance" "instances" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids =var.security_groups
+  tags                   ={
+    Name = var.name
+  }
 }
 
 
+resource "aws_route53_record" "record" {
+  zone_id = var.zone_id
+  name    = "${var.name}-dev.poornadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instances.private_ip]
+}
+
+
+resource "null_resource" "ansible" {
+  depends_on = [
+  aws_route53_record.record
+  ]
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/practice/ansible-roboshop
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.poornadevops.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+EOF
+  }
+}
 
 
 
