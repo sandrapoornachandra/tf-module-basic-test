94f0b67a487fe67e7bc63c257fb1c90b16b92edd
s poorna chandra <poornanaidu1@gmail.com>
2023-12-27 12:33:26 +0530

    practice

--- module/main.tf
+++ module/main.tf
@@ -23,11 +23,10 @@ resource "null_resource" "ansible" {
 
   provisioner "local-exec" {
     command = <<EOF
-cd /home/centos/roboshop-ansible
+cd /home/centos/practice/ansible-roboshop
 git pull
 sleep 30
-ansible-playbook -i ${var.name}-dev.poornadevops.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321
--e component=${var.name}
+ansible-playbook -i ${var.name}-dev.poornadevops.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
 EOF
   }
 }
