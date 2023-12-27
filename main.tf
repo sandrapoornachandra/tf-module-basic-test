133485af777cf4563a332d0b6de6623acb85b84f
s poorna chandra <poornanaidu1@gmail.com>
2023-12-27 11:39:39 +0530

    practice

---
+++ main.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "instances" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids =var.security_groups
+  tags                   ={
+    Name = var.name
+  }
+}
+
+
+resource "aws_route53_record" "record" {
+  zone_id = var.zone_id
+  name    = "${var.name}-dev.poornadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instances.private_ip]
+}
+
+
+resource "null_resource" "ansible" {
+  depends_on = [
+  aws_route53_record.record
+  ]
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/ansible-roboshop-v1
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.poornadevops.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+EOF
+  }
+}
+#
+
+
+
+
+
+
---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "zone_id" {}
+variable "security_groups" {}
+variable "name" {}
+variable "instance_type" {}
+
+
+
+
